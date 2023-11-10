cfc27b825769a7a2c0d60c98cacc13be1e69677c
originalAngelMv <143576121+originalAngelMv@users.noreply.github.com>
2023-11-10 11:27:56 -0600

    Imprimir nota Morales Ventura Angel

--- PIA_EDyP_Nov2023.py
+++ PIA_EDyP_Nov2023.py
@@ -41,3 +41,33 @@ except:
 finally:
     if (conn):
         conn.close()
+        
+def imprimir_nota(id_nota):
+    with sqlite3.connect('notas.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as conn:
+        mi_cursor = conn.cursor()
+        mi_cursor.execute("SELECT n.id_nota, n.fecha_nota, c.nombre_cliente, c.RFC_cliente, c.correo_cliente,n.monto_a_pagar, dn.id_servicio, s.nombre_servicio, s.costo_servicio\
+            FROM notas n\
+            INNER JOIN clientes c ON n.id_cliente = c.id_cliente\
+            INNER JOIN detalles_notas dn ON n.id_nota = dn.id_nota\
+            INNER JOIN servicios s ON dn.id_servicio = s.id_servicio\
+            WHERE n.id_nota = ?", (id_nota,))
+
+        resultados = mi_cursor.fetchall()
+
+        id_nota, fecha_nota, nombre_cliente, RFC_cliente, correo_cliente, monto_a_pagar = resultados[0][:6]
+
+        print(f"ID Nota: {id_nota}")
+        print(f"Fecha de Nota: {fecha_nota.strftime('%d-%m-%Y')}")
+        print(f"Cliente: {nombre_cliente}")
+        print(f"RFC: {RFC_cliente}")
+        print(f"Correo: {correo_cliente}")
+        print("*" * 60)
+
+        print(f"{'ID Servicio':<15} {'Nombre del Servicio':<25} {'Costo':<10}")
+        print("-" * 50)
+
+        for resultado in resultados:
+            id_servicio, nombre_servicio, costo_servicio = resultado[6:]
+            print(f"{id_servicio:<15} {nombre_servicio:<25} {costo_servicio:<10.2f}")
+
+        print(f"Monto a Pagar: {monto_a_pagar:.2f}")
